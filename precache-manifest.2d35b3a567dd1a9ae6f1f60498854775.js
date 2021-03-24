721c778b27c96046074fdfa0acfcaa64f07e6fe3
Alexey Khromets <akhromets@webinerds.com>
2021-03-24 12:50:46 +0200

    Updates

---
+++ precache-manifest.2d35b3a567dd1a9ae6f1f60498854775.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a989f361929149c4cfeabb0483cd03",
+    "url": "/slate-webview-demo/index.html"
+  },
+  {
+    "revision": "86799d47ac36804215b1",
+    "url": "/slate-webview-demo/static/js/2.d10c13e1.chunk.js"
+  },
+  {
+    "revision": "a894db0417d8a564c5da",
+    "url": "/slate-webview-demo/static/js/main.15478e30.chunk.js"
+  },
+  {
+    "revision": "1db5799987bbb516f0dc",
+    "url": "/slate-webview-demo/static/js/runtime~main.1b24e842.js"
+  }
+]);
